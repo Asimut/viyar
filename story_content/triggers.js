72d6b6b1c5313246974ac7318cfcf7c0f64bd335
Oleh <asimut007@gmail.com>
2024-06-10 16:50:40 +0300

    Upload

---
+++ story_content/triggers.js
@@ -0,0 +1,89 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5saMnLTR1OI":
+        Script1();
+        break;
+      case "6lMIHLPJnf4":
+        Script2();
+        break;
+      case "6AjbddS1Wu8":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('6QzRSfFjTkF');
+const duration = 750;
+const easing = 'linear';
+const id = '5bcYXvmaESQ';
+const shrinkAmount = 0.3;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: `${1 - shrinkAmount}` }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  player.once(() => {
+const target = object('630a2sn6U9C');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6kUkOFdbVd0';
+const shakeAmount = 2;
+const delay = 20000;
+addToTimeline(
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script3 = function()
+{
+  const target = object('630a2sn6U9C');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6kUkOFdbVd0';
+const shakeAmount = 2;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
